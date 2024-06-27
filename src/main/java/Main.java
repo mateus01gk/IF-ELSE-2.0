98d5a4f808a8c8f9a78cb3a2c907de8ae983f5c4
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-06-27 00:38:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+    System.out.println("vamos fazer um programa que identifique um numero inteiro");
+    System.out.println("");
+    System.out.println("e esse programa irá classificar ele como impar ou como par");
+    System.out.println("");
+    System.out.println("digite um numero inteiro");
+    Double numero = sc.nextDouble();
+
+
+
+    if(numero/2 = 0){
+      System.out.println("o numero é par");
+    }
+if(numero/2 = 1){
+  System.out.println("o numero é impar");
+}
+     
+
+
+
+
+
+
+
+    sc.close();
+  }
+
+  
+}
